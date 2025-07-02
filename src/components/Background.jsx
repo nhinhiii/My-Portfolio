e9eeb81b87cc31a76bcbf8e321ef9d80f78ea25c
nhinhiii <nhinguyenyen0908@gmail.com>
2025-07-02 16:08:44 -0700

    add more color to the spotlight depends on the theme

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,15 +3,21 @@ import { Spotlight } from "./ui/Spotlight";
 
 export const Background = () => {
   return (
-    <div className="pb-20 pt-36 bg-black">
+    <div className="pb-20 pt-36 relative">
       <Spotlight
-        className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-        fill="white"
+        className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen absolute"
+        fill="var(--spot1)"
       />
 
-      <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple" />
+      <Spotlight
+        className="h-[70vh] w-[45vw] top-15 left-full absolute"
+        fill="var(--spot2)"
+      />
 
-      <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      <Spotlight
+        className="left-72 top-28 h-[70vh] w-[40vw] absolute"
+        fill="var(--spot3)"
+      />
     </div>
   );
 };
